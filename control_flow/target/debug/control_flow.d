ebd81cc72428e92c924cc68e1c2e8584edc57b61
Robert Hanna <rdhanna@gmail.com>
2023-07-23 10:44:02 -0400

    delelte .gitignore files

--- control_flow/target/debug/control_flow.d
+++
@@ -1,1 +0,0 @@
-/mnt/d/dev/Rust/rustbook/projects/control_flow/target/debug/control_flow: /mnt/d/dev/Rust/rustbook/projects/control_flow/src/main.rs
--- control_flow/target/debug/deps/control_flow-09707857d24443d0.d
+++
@@ -1,5 +0,0 @@
-/mnt/d/dev/Rust/rustbook/projects/control_flow/target/debug/deps/control_flow-09707857d24443d0: src/main.rs
-
-/mnt/d/dev/Rust/rustbook/projects/control_flow/target/debug/deps/control_flow-09707857d24443d0.d: src/main.rs
-
-src/main.rs:

60af83585977d2a2400a08b814bbd81761805c8b
Robert Hanna <rdhanna@gmail.com>
2023-07-23 14:03:52 -0400

    finished control_flow section

--- control_flow/src/main.rs
+++ control_flow/src/main.rs
@@ -1,9 +1,49 @@
 fn main() {
-    let number = 3;
+    // let number = 7;
 
-    if number < 5 {
-        println!("condition was true");
-    } else {
-        println!("condition was false");
+    // if number < 5 {
+    //     println!("condition was true");
+    // } else {
+    //     println!("condition was false");
+    // }
+
+    // let num = 6;
+
+    // if num % 4 == 0 {
+    //     println!("Number is divisible by 4")
+    // } else if num % 3 == 0 {
+    //     println!("Number is divisible by 3")
+    // } else if num % 2 == 0 {
+    //     println!("Number is divisible by 2")
+    // } else { 
+    //     println!("Number is not divisible by 4, 3, or 2")
+    // }
+
+    // loop conditions
+    // let mut cnt = 0;
+
+    // let result = loop {
+    //     cnt += 1;
+
+    //     if cnt == 10 {
+    //         break cnt * 2;
+    //     }
+    // };
+    // println!("The result is {}", result)
+
+    // for loop via iteration
+    // let a = [10, 20, 30, 40, 50];
+    // for element in a.iter() {
+    //     println!("The value is : {}", element)
+    // }
+
+    // for loop via range (forward)
+    for num in 1..4{
+        println!("num: {}",num)
+    }
+
+    // for loop via range (reverse) 
+    for num in (1..4).rev() {
+        println!("num: {}",num)
     }
 }

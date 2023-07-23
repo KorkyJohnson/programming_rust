e4cee65b0602681172bad5ffe5016ce912718755
Robert Hanna <rdhanna@gmail.com>
2023-07-23 10:00:02 -0400

    functions and expressions

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -17,18 +17,39 @@ fn main() {
     // let spaces = spaces.len();
 
     // primative types
-    let sum = 5 + 10;
-    let diff = 95.5 - 4.3;
-    let product = 4 * 30;
-    let quot = 56.7 / 32.2;
-    let remainder = 43 % 5;
-    let character = 't';
-    let text = "text";
-    println!("Sum: {}, Diff: {}, Product: {}, Quotient: {}, Remainder: {}\n", sum, diff, product, quot, remainder);
-    println!("Character: {}, String: {}\n", character, text);
+    // let sum = 5 + 10;
+    // let diff = 95.5 - 4.3;
+    // let product = 4 * 30;
+    // let quot = 56.7 / 32.2;
+    // let remainder = 43 % 5;
+    // let character = 't';
+    // let text = "text";
+    // println!("Sum: {}, Diff: {}, Product: {}, Quotient: {}, Remainder: {}\n", sum, diff, product, quot, remainder);
+    // println!("Character: {}, String: {}\n", character, text);
 
-    // tuple example
-    let tup = (500, 6.4, 1);
-    let (x, y, z) = tup;
-    println!("The value of x is : {}\nThe value of y is : {}\nThe value of z is : {}", tup.0, tup.1, tup.2);
+    // // tuple example
+    // let tup = (500, 6.4, 1);
+    // let (x, y, z) = tup;
+    // println!("The value of x is : {}\nThe value of y is : {}\nThe value of z is : {}", tup.0, tup.1, tup.2);
+
+    // // arrays
+    // let a = [1,2,3,4,5];
+    // let first = a[0];
+    // let second = a[1];
+    // println!("First: {}, Second: {}", first, second);
+
+    // functions
+    // println!("Hello World");
+    // another_function("Hello parameter".to_string());
+
+    // expressions
+    let y = {
+        let x = 3;
+        x + 1
+    };
+    println!("The value of y is: {}", y);
 }
+
+// fn another_function(parm: String){
+//     println!("{}",parm);
+// }

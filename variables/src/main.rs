9b6b09080a47cbc3472426018bcab8167febb151
Robert Hanna <rdhanna@gmail.com>
2023-07-23 08:26:24 -0400

    more playing w/ /variables (tuples)

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -13,7 +13,22 @@ fn main() {
 
     // println!("The value of x is {}", x); 
 
-    let spaces = "   ";
-    let spaces = spaces.len();
+    // let spaces = "   ";
+    // let spaces = spaces.len();
 
+    // primative types
+    let sum = 5 + 10;
+    let diff = 95.5 - 4.3;
+    let product = 4 * 30;
+    let quot = 56.7 / 32.2;
+    let remainder = 43 % 5;
+    let character = 't';
+    let text = "text";
+    println!("Sum: {}, Diff: {}, Product: {}, Quotient: {}, Remainder: {}\n", sum, diff, product, quot, remainder);
+    println!("Character: {}, String: {}\n", character, text);
+
+    // tuple example
+    let tup = (500, 6.4, 1);
+    let (x, y, z) = tup;
+    println!("The value of x is : {}\nThe value of y is : {}\nThe value of z is : {}", tup.0, tup.1, tup.2);
 }

af61349d3aa6e476a82335c2aba6e2304eba5901
Robert Hanna <rdhanna@gmail.com>
2023-08-26 08:14:15 -0400

    initial menu creation for ex3

--- collection_exercises/src/main.rs
+++ collection_exercises/src/main.rs
@@ -1,5 +1,5 @@
-use collection_exercises::ex1::{median::get_median, mode::get_mode};
-use collection_exercises::ex2::pig_latin::to_pig_latin;
+// use collection_exercises::ex1::{median::get_median, mode::get_mode};
+// use collection_exercises::ex2::pig_latin::to_pig_latin;
 use std::io;
 
 fn main() {
@@ -26,15 +26,64 @@ fn main() {
     // the word and “ay” is added, so “first” becomes “irst-fay.” Words that start with a vowel
     // have “hay” added to the end instead (“apple” becomes “apple-hay”). Keep in mind the details
     // about UTF-8 encoding!
-    let mut user_string = String::new();
+    // let mut user_string = String::new();
 
-    println!("Enter a sentence and I will transform it into pig latin:");
-    io::stdin()
-        .read_line(&mut user_string)
-        .expect("You didn't enter anything");
+    // println!("Enter a sentence and I will transform it into pig latin:");
+    // io::stdin()
+    //     .read_line(&mut user_string)
+    //     .expect("You didn't enter anything");
 
-    println!(
-        "Your sentence in pig latin: {:?}",
-        to_pig_latin(user_string)
-    );
+    // println!(
+    //     "Your sentence in pig latin: {:?}",
+    //     to_pig_latin(user_string)
+    // );
+
+    // *************************************************************************************
+    // * Exercise 3
+    // *************************************************************************************
+    // Using a hash map and vectors, create a text interface to allow a user to add employee
+    // names to a department in a company. For example, “Add Sally to Engineering” or
+    // “Add Amir to Sales.” Then let the user retrieve a list of all people in a department
+    // or all people in the company by department, sorted alphabetically.
+
+    // Step 1   - Need to request user input
+    //          - Add or Disp people
+    //          - parse 'Add Bob to Programmers
+    //          - Bob is name (key) / Programmers is job (value)
+    //          - need to split
+    //          - move all words to vector?
+    // Step 2   - Need to move K/V to hashmap
+    // Step 3   - When display, then show list of users alphaBetically
+    //          - If none, then display 'Noone has been added"
+    // Bonus    - Allow user to subtract people from the hashmap as well
+    loop {
+        let mut user_input = String::new();
+
+        println!("********************************************************");
+        println!("*                                                      *");
+        println!("*          Please make a selection below               *");
+        println!("*                                                      *");
+        println!("*              (A)dd a employee                        *");
+        println!("*              (P)rint all employees                   *");
+        println!("*              (Q)uit program                          *");
+        println!("*                                                      *");
+        println!("********************************************************");
+        io::stdin()
+            .read_line(&mut user_input)
+            .expect("You didn't enter anything");
+
+        if let Some(user_choice) = user_input.trim().chars().next() {
+            match user_choice.to_ascii_lowercase() {
+                'a' => println!("User wants to add"),
+                'p' => println!("User wants to print"),
+                'q' => {
+                    println!("Goodbye :)");
+                    break;
+                }
+                _ => println!("Invalid selection"),
+            }
+        } else {
+            println!("No valid input entered");
+        }
+    }
 }
